ac09a14592394add3825732a61dd2ca6edec517c
caioh230 <caiohchagas92@gmail.com>
2024-05-19 21:53:05 -0300

    Alteração de todo o código-fonte e inserção de texto no README

--- Código-Fonte/springboot-api-ebook-master/src/main/java/com/example/springboot/dtos/APIRecordDto.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.springboot.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-//Código DTO para transferir dados para o banco de dados
-public record APIRecordDto(@NotBlank String name,  @NotBlank String endpoint, @NotBlank String metodos, @NotBlank String parametros) {
-}
